64ca3f278321e71e979e7934edea0f090c1262c8
Kevin Miller <keveemiller@gmail.com>
2019-11-01 09:58:37 -0300

    Fixed fonts, cached NOAA data for easier local dev

--- plugins/gatsby-source-noaa-tides/gatsby-node.js
+++ plugins/gatsby-source-noaa-tides/gatsby-node.js
@@ -1,5 +1,10 @@
 const request = require('sync-request')
 const crypto = require('crypto')
+const fs = require('fs')
+
+if (!fs.existsSync('./.noaa-cache')) {
+  fs.mkdirSync('./.noaa-cache')
+}
 
 exports.sourceNodes = ({ actions, createNodeId, reporter }, configOptions) => {
   const { createNode } = actions
@@ -15,32 +20,43 @@ exports.sourceNodes = ({ actions, createNodeId, reporter }, configOptions) => {
         type: `NoaaStation`
       }
     }
-    const harmonics = request(
-      'GET',
-      `https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations/${stationId}/harcon.json?units=metric`
-    )
-    station.harmonics = JSON.parse(harmonics.getBody())
-    const levels = request(
-      'GET',
-      `https://tidesandcurrents.noaa.gov/api/datagetter?date=recent&station=${stationId}&product=predictions&datum=MTL&time_zone=gmt&units=metric&format=json`
-    )
-    station.levels = JSON.parse(levels.getBody())
-    const datum = request(
-      'GET',
-      `https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations/${stationId}/datums.json?units=metric`
-    )
-    station.datum = JSON.parse(datum.getBody())
-    const info = request(
-      'GET',
-      `https://tidesandcurrents.noaa.gov/mdapi/latest/webapi/stations/${stationId}.json?units=english`
-    )
-    station.info = JSON.parse(info.getBody())
-    const observations = request(
-      'GET',
-      `https://tidesandcurrents.noaa.gov/api/datagetter?date=recent&station=${stationId}&product=water_level&datum=MTL&time_zone=gmt&units=metric&format=json`
-    )
-    station.observations = JSON.parse(observations.getBody())
 
+    const filePath = `./.noaa-cache/${stationId}.json`
+    if (fs.existsSync(filePath)) {
+      const cachedStation = JSON.parse(fs.readFileSync(filePath))
+      station.harmonics = cachedStation.harmonics
+      station.levels = cachedStation.levels
+      station.datum = cachedStation.datum
+      station.info = cachedStation.info
+      station.observations = cachedStation.observations
+    } else {
+      const harmonics = request(
+        'GET',
+        `https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations/${stationId}/harcon.json?units=metric`
+      )
+      station.harmonics = JSON.parse(harmonics.getBody())
+      const levels = request(
+        'GET',
+        `https://tidesandcurrents.noaa.gov/api/datagetter?date=recent&station=${stationId}&product=predictions&datum=MTL&time_zone=gmt&units=metric&format=json`
+      )
+      station.levels = JSON.parse(levels.getBody())
+      const datum = request(
+        'GET',
+        `https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations/${stationId}/datums.json?units=metric`
+      )
+      station.datum = JSON.parse(datum.getBody())
+      const info = request(
+        'GET',
+        `https://tidesandcurrents.noaa.gov/mdapi/latest/webapi/stations/${stationId}.json?units=english`
+      )
+      station.info = JSON.parse(info.getBody())
+      const observations = request(
+        'GET',
+        `https://tidesandcurrents.noaa.gov/api/datagetter?date=recent&station=${stationId}&product=water_level&datum=MTL&time_zone=gmt&units=metric&format=json`
+      )
+      station.observations = JSON.parse(observations.getBody())
+    }
+    fs.writeFileSync(filePath, JSON.stringify(station))
     station.internal.contentDigest = crypto
       .createHash(`md5`)
       .update(`noaa-station-${stationId}`)
